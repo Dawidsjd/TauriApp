470d5d91869449d7053fdadc9e2b89631dec17c9
atek <53567837+Dawidsjd@users.noreply.github.com>
2024-04-29 21:59:20 +0200

    Adding Loading component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import BitcoinChart from './components/Chart/BitcoinChart';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import TradePanel from './components/TradePanel/TradePanel';
+import LoaderStart from './assets/Loader/LoaderStart';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      
+
+      {/* Zrobic funkcjonalnosc do Loadingu */}
+      {/* <LoaderStart /> */}
+
       <Navbar/>
 
       <div className="relative flex items-center justify-center mt-32 ml-4">
